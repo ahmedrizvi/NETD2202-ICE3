7c59aa94afeed7cb1239fb333afae74141b989e8
ahmedrizvi <ahmed.riz117@gmail.com>
2020-06-25 11:50:16 -0400

    Initial Commit

---
+++ WindowsFormICE03/Form1.vb
@@ -0,0 +1,6 @@
+﻿' Generic Windows form to be pushed to NETD2202-ICE3 repository.
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
